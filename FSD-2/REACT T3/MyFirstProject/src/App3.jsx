2229d864ea3bb2cd3e36f9fb97c7932689490f27
Harshh1143 <mistryharsh5416@gmail.com>
2026-05-19 12:33:04 +0530

    Resusable Components

---
+++ FSD-2/REACT T3/MyFirstProject/src/App3.jsx
@@ -0,0 +1,13 @@
+export default function App3() {
+  return (
+    <>
+      <ol>
+        <li>Fruit</li>
+        <li>Vegetables</li>
+        <li>Hello</li>
+        <li>Dairy</li>
+        <li>Grains</li>
+      </ol>
+    </>
+  );
+}
