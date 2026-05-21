72095e759d7e8768367987efa749fb7491c0dd8b
Harshh1143 <mistryharsh5416@gmail.com>
2026-05-21 12:47:31 +0530

    .gitignore

--- PYTHON-2/T3/DJANGO/L1/L1/urls.py
+++ PYTHON-2/T3/DJANGO/L1/L1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app1.views import hello
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/',hello)
 ]
