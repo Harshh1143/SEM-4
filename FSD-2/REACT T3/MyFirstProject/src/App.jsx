12ef02e6a7a84ec793aa1a88c59cdab392dfa864
Harshh1143 <mistryharsh5416@gmail.com>
2026-05-22 09:26:07 +0530

    Add CN component and integrate into App; minor adjustments in App2

--- FSD-2/REACT T3/MyFirstProject/src/App.jsx
+++ FSD-2/REACT T3/MyFirstProject/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import App2 from "./App2";
 import App3 from "./App3";
+import CN from "./CN";
 export default function App() {
   const name = 'abc';
   const mark = [1,2,3]
@@ -9,6 +10,8 @@ export default function App() {
       <h1>Welcome {name.toUpperCase()}</h1>
       <h1>{mark.join(', ')}</h1>
       <h1>My first code </h1>
+      <CN name='value' age='23'/>
+      <CN name={name} age={23}/>
       <App2 />
       <App2 />
       <App2 />
---
+++ FSD-2/REACT T3/MyFirstProject/src/CN.jsx
@@ -0,0 +1,8 @@
+export default function CN (p){
+  return (
+    <>
+    <h1>Name: {p.name}</h1>
+    <h1>Age: {p.age}</h1>
+    </>
+  )
+}
