d09fd7552911fcbceaa276bdd67d04ee0770deb9
Harshh1143 <mistryharsh5416@gmail.com>
2026-05-26 13:29:57 +0530

    Using hooks ( useState ) and creating several code for understanding it

--- FSD-2/REACT T3/MyFirstProject/src/components/MainR.jsx
+++ FSD-2/REACT T3/MyFirstProject/src/components/MainR.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
+import Usi from '../Usi.jsx'
+import Color from '../Color.jsx'
+import Task2 from '../Task2.jsx'
 
 export default function MainR() {
   return (
@@ -28,6 +31,21 @@ export default function MainR() {
               Contact
             </Link>
           </li>
+          <li>
+            <Link style={styles.link} to="/usi">
+              Us1
+            </Link>
+          </li>
+          <li>
+            <Link style={styles.link} to="/color">
+              Color
+            </Link>
+          </li>
+          <li>
+            <Link style={styles.link} to="/task2">
+              task2
+            </Link>
+          </li>
         </ul>
       </nav>
 
@@ -36,6 +54,9 @@ export default function MainR() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/usi" element={<Usi />} />
+        <Route path="/color" element={<Color />} />
+        <Route path="/task2" element={<Task2 />} />
       </Routes>
     </Router>
   );
