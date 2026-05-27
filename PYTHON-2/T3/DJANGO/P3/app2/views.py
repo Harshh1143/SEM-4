e97036aa24f54d5e32aa486104038bea7996b5e1
Harshh1143 <mistryharsh5416@gmail.com>
2026-05-27 13:14:19 +0530

    Add dynamic content to hello view and update base template structure

--- PYTHON-2/T3/DJANGO/P3/app2/views.py
+++ PYTHON-2/T3/DJANGO/P3/app2/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 def contact(request):
     return render(request,'contact.html') 
 def hello(request):
-    return render(request,'base.html')
+    name = 'harsh'
+    number = 8
+    return render(request,'base.html',{'n':name,'num':number})
