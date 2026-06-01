03b7adafc6bcb6f6c437412e621453fe721ffc60
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-01 09:35:28 +0530

    Refactor code structure for improved readability and maintainability

--- PYTHON-2/T3/DJANGO/B4/app1/models.py
+++ PYTHON-2/T3/DJANGO/B4/app1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    roll=models.IntegerField()
+    profile=models.URLField(max_length=200)
+    email=models.EmailField(max_length=254)
+    marks=models.IntegerField()
+    birthdate = models.DateField(auto_now=False)
+
+    def __str__(self):
+        return self.name
--- PYTHON-2/T3/DJANGO/B4/app1/views.py
+++ PYTHON-2/T3/DJANGO/B4/app1/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from app1.models import Student
 # Create your views here.
+def home(request):
+    students = Student.objects.all()
+    return render(request,'home.html', {'students': students})
