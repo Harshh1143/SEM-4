07ac27c7ea04dbec0ef4dec2374f23b12e4fbdf2
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-02 11:56:17 +0530

    Updating views.py

--- PYTHON-2/T3/DJANGO/B4/app1/views.py
+++ PYTHON-2/T3/DJANGO/B4/app1/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from app1.models import Student
 # Create your views here.
 def home(request):
-    students = Student.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        students = Student.objects.filter(name__icontains=search_query)
+    else:
+        students = Student.objects.all()
     return render(request,'home.html', {'students': students})
