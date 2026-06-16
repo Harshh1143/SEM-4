4699ed1a120282e632d27e948dac7d2a8d63e749
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-16 14:56:40 +0530

    Add initial Django project structure for pb434 and pb435
    - Created manage.py for both pb434 and pb435 projects.
    - Added __init__.py files to both projects.
    - Implemented ASGI and WSGI configurations for both projects.
    - Set up settings.py with default configurations including database settings.
    - Established URL routing in urls.py for both projects.
    - Included necessary middleware and installed apps in settings.py.
    - Generated initial cache files for Python bytecode.

---
+++ PYTHON-2/T3/DjangoSelf/pb434/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Contact 
+# Create your views here.
+
+def contact_list(request) : 
+    contacts = Contact.objects.all()
+    return render(
+        request,
+        'contact.html',
+        {'contacts': contacts }
+    )
