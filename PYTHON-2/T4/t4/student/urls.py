7fe24d6f0113bf6bd52badb7c0777b8510da16a8
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-24 12:54:31 +0530

    feat: Implement student addition functionality with form and template

--- PYTHON-2/T4/t4/student/urls.py
+++ PYTHON-2/T4/t4/student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import home,info
+from .views import home,info,add
 urlpatterns = [
     path("",home,name='home'),
-    path('info/<int:id>/',info,name='info')
+    path('info/<int:id>/',info,name='info'),
+    path('add/',add,name='add')
 ]
--- PYTHON-2/T4/t4/student/views.py
+++ PYTHON-2/T4/t4/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
 from .models import Student
 # Create your views here.
 def home(requests) :
@@ -8,3 +8,13 @@ def home(requests) :
 def info(requests,id):
     marks = get_object_or_404(Student,id=id)
     return render(requests,'info.html',{'i':marks})
+
+def add(requests) :
+    if requests.method=='POST' :
+        name = requests.POST['name']
+        score = requests.POST['score']
+        sub = requests.POST['sub']
+        Student.objects.create(name=name,score=score,sub=sub)
+
+        return redirect('home')
+    return render(requests,'add.html')
