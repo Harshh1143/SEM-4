a36893b5a64bb3344052bf0fbf66a4aaab89c789
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-25 12:18:44 +0530

    feat: Implement user registration and login functionality with corresponding templates

--- PYTHON-2/T4/t4/dashboard/views.py
+++ PYTHON-2/T4/t4/dashboard/views.py
@@ -6,3 +6,27 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def dashboard(requests) :
     return render (requests,'dashboard.html')
+
+def register(requests) :
+    if requests.method=='POST' :
+        form = UserCreationForm(requests.POST)
+
+    if form.is_valid() :
+        user = form.save()
+        login(requests,user)
+        return redirect('dashboard')
+    else :
+        form = UserCreationForm()
+    return render(requests,'register.html',{'form':form})
+
+def user_login(requests) :
+    if requests.user.is_authenticated :
+        return redirect('dashboard')
+    if requests.method=='POST' :
+        form = AuthenticationForm(requests,data=requests.POST)
+        if form.is_valid():
+            login(requests,form.get_user)
+            return redirect('home')
+    else :
+            form - AuthenticationForm()
+    return render(requests,'login.html',{'form':form})
