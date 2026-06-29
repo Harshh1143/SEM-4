1d267d19c01a37e8a32591361108a8b9150d05fa
Harshh1143 <mistryharsh5416@gmail.com>
2026-06-29 09:51:37 +0530

    feat: Implement search functionality for players in home view

--- PYTHON-2/T4/playerproject/playerapp/views.py
+++ PYTHON-2/T4/playerproject/playerapp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from .models import Player
 # Create your views here.
 def home(requests) :
-    data = Player.objects.all()
-    return render(requests,'home.html',{'data':data})
+    search = requests.GET.get('name','')
+    if search :
+        play = Player.objects.filter(name__icontains=search)
+    else :
+        play = Player.objects.all()
+    return render(requests,'home.html',{'data':play})
 
 def welcome(requests) :
     return render(requests,'welcome.html')
