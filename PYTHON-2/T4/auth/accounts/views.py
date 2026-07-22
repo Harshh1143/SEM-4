d43e62187786efeff9baa52f511b758095cbd3f5
Harshh1143 <mistryharsh5416@gmail.com>
2026-07-22 14:31:36 +0530

    Initialize Django project structure with essential files and settings
    - Created __init__.py to mark the directory as a Python package.
    - Added asgi.py for ASGI configuration.
    - Created settings.py with default settings for the Django project.
    - Added urls.py for URL routing configuration.
    - Implemented wsgi.py for WSGI configuration.
    - Created manage.py for administrative tasks.

--- PYTHON-2/T4/auth/accounts/views.py
+++ PYTHON-2/T4/auth/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect 
-from django.contrib.auth.form import UserCreationForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login, logout 
 from django.contrib.auth.decorators import login_required 
 
@@ -10,12 +10,12 @@ def signup_view(request) :
             user = form.save()
             login(request,user)
             return redirect('dashboard')
-        else :
-            form = UserCreationForm()
-        return render(request,'signup.html',{'form':form})
+    else :
+        form = UserCreationForm()
+    return render(request,'signup.html',{'form':form})
 
 def login_view(request):
-    if request.user.is_Authenticated() :
+    if request.user.is_authenticated :
         return redirect("dashboard")
     if request.method == "POST" :
         form = AuthenticationForm(request,data=request.POST)
@@ -23,14 +23,14 @@ def login_view(request):
             user = form.get_user()
             login(request,user)
             return redirect('dashboard')
-        else :
-            form = AuthenticationForm()
-        return render(request,'login.html',{'form',form})
+    else :
+        form = AuthenticationForm()
+    return render(request,'login.html',{'form':form})
 
 @login_required
 def dashboard(request):
     return render(request,'dashboard.html')
 
-def logout(request):
+def logout_view(request):
     logout(request)
     return redirect("login")
