d7c0cfb67618ae94bdcb2163ede9e1517e62aab9
Harshh1143 <mistryharsh5416@gmail.com>
2026-07-25 15:03:35 +0530

    Add initial implementation of Django sample application
    - Created __pycache__ files for Python modules: __init__, admin, apps, models, urls, views, and migrations.
    - Added HTML templates for home, about, contact, and 404 not found pages.
    - Implemented URL routing for home, about, and contact views.
    - Defined basic structure for the home page with navigation links.

--- IPE/PYTHON-2/SampleApp/sample/views.py
+++ IPE/PYTHON-2/SampleApp/sample/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'sample/home.html')
+
+def about(request) :
+    return render(request,'sample/about.html')
+
+def contact(request) : 
+    return render(request,'sample/contact.html')
