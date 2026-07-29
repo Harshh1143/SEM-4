b8b788e340b6d3a6f84bea2ff85498ae01d7e577
Harshh1143 <mistryharsh5416@gmail.com>
2026-07-29 15:25:09 +0530

    fix: correct variable names and formatting in Kaprekar number script

--- IPE/FSD-2/PYQ/P1.js
+++ IPE/FSD-2/PYQ/P1.js
@@ -1,17 +1,22 @@
 // Write a NodeJS script to take 2 elements 1 & 1000 using file system module & find
 // Kaprekar numbers between them. A Kaprekar number is a number whose square when
 // divided into two parts and such that sum of parts is equal to the original number and
-// none of the parts has value 0. 
+// none of the parts has value 0.
 
-const fs = require("fs")
-const data = fs.readFileSync("input.txt",'utf-8').trim().split(" ")
+const fs = require("fs");
+const data = fs.readFileSync("input.txt", "utf-8").trim().split(" ");
 
-console.log(data)
-const fn = Number(data[0])
-const ln = Number(data[1])
+console.log(data);
+const fn = Number(data[0]);
+const ln = Number(data[1]);
 
-for (let i = start; i<=ln; i++ ) {
-    const sq = i*i 
-    const digit = i.toString().length()
-    
+for (let i = fn; i <= ln; i++) {
+  let sq = i * i;
+  let digits = i.toString().length;
+  let divisor = Math.pow(10, digits);
+  let left = Math.floor(sq / divisor);
+  let right = sq % divisor;
+  if (left > 0 && right > 0 && left + right === i) {
+    console.log(i);
+  }
 }
